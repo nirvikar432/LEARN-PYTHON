45158515012d92b3a300b486133458c2324018f6
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-09-07 01:36:19 +0530

    Initial Commit

---
+++ TryExceptExceptionHandling.py
@@ -0,0 +1,11 @@
+num1 = input("Enter num 1: ")
+num2 = input("Enter num 2: ")
+try:
+    print("The sum of these two numbers is", int(num1)+int(num2))
+except Exception as e:  #any name can be used not only e
+    print(e)
+
+
+
+print("This line is very important")
+
