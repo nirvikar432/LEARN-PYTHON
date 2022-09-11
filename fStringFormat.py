2dba0d8961088d650b0a3516bb2a457a8d43455f
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-09-11 18:09:36 +0530

    Initial Commit

---
+++ fStringFormat.py
@@ -0,0 +1,13 @@
+import math
+me = "Nirvikar"
+a1 =20
+a = "this is %s and my birthday is on %s"%(me, a1)
+# a = "This is {1} {0}"
+# b = a.format(me, a1)
+# print(b)
+c = f"this is {me} {a1} {math.cos(65)}"
+print(a)
+print(c)
+
+
+  
